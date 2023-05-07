5e6e4501d1d105a3c970911f78e282360127bc6d
Danny <dannyschapeit@slynite.com>
2023-05-07 10:22:29 +0200

    Update footer link

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Footer() {
     return(
         <>
-            <p className="self-end">Proudly created by <Link href={'https://github.com/slynite/status'} target="_blank" className='underline'>Slynite</Link> with <Link href={'https://nextjs.org'} target="_blank" className='underline'>Next.js</Link>.</p>
+            <p className="self-end">Proudly created by <Link href={'https://github.com/slynite/status-page'} target="_blank" className='underline'>Slynite</Link> with <Link href={'https://nextjs.org'} target="_blank" className='underline'>Next.js</Link>.</p>
             <p className="self-end">Hosted on <Link href={'https://vercel.com/legal/privacy-policy'} target="_blank" className='underline'>Vercel</Link>.</p>
         </>
     )
